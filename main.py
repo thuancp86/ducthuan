6bdefb45e0a708c49944907b070614b1bd58d84c
thuancp86 <thuancp86@gmail.com>
2016-03-27 12:52:08 +0700

    Added files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Module: default
-# Author: Roman V. M.
-# Created on: 28.11.2014
+# Author: Thuận.
+# Created on: 26.3.2016
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import sys
@@ -18,45 +18,62 @@ _handle = int(sys.argv[1])
 # Here we use a fixed set of properties simply for demonstrating purposes
 # In a "real life" plugin you will need to get info and links to video files/streams
 # from some web-site or online service.
-VIDEOS = {'Animals': [{'name': 'Crab',
-                       'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
-                       'video': 'http://www.vidsplay.com/vids/crab.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Alligator',
-                       'thumb': 'http://www.vidsplay.com/vids/alligator.jpg',
-                       'video': 'http://www.vidsplay.com/vids/alligator.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Turtle',
-                       'thumb': 'http://www.vidsplay.com/vids/turtle.jpg',
-                       'video': 'http://www.vidsplay.com/vids/turtle.mp4',
-                       'genre': 'Animals'}
+VIDEOS = {'Onetv (Chỉ Mạng FPT)': [{'name': 'VTV1 HD',
+                       'thumb': 'http://download028.fshare.vn/dl/kQRrpyuRtQ7DiFzzEcm+SUZYGG+5Z23oqguklwDwaujX2PEdzCywzxIE1SKeMk+36Eju21tan76S3v-4/VTV1%20HD.jpg',
+                       'video': 'udp://@225.1.2.249:30120',
+                       'genre': 'Truyền Hình'},
+                      {'name': 'VTV3 HD',
+                       'thumb': 'http://download031.fshare.vn/dl/jsCm246bVbhwWBK6o4TaTmGoYOQJykiJdEwNjcvH6LExC4XVKvymtf0zXB46BrZVVVenemIlyYb98A-v/VTV3%20HD.jpg',
+                       'video': 'udp://@225.1.2.247:30120',
+                       'genre': 'Truyền Hình'},
+					  {'name': 'VTC13-iTV HD',
+                       'thumb': 'http://download031.fshare.vn/dl/DNU4sjHs3jXp-amJKioTxH7TpqaQl3xYSirH1z+EvlikStP6At+LYKGmNjuCLHft5c1fgIG6CSSw8iQx/ITV%20HD.png',
+                       'video': 'udp://@225.1.2.250:30120',
+                       'genre': 'Truyền Hình'},
+                      {'name': 'HBO HD',
+                       'thumb': 'http://download028.fshare.vn/dl/oWokKZrk6wOeIZHIppbj9kQvoMPAlwO8pZqUSoJ0YnjOPj2XRslw1Hp3kJP-qSMY7VodC0KitXlKy6Yy/HBO%20HD.jpg',
+                       'video': 'udp://@225.1.2.233:30120',
+                       'genre': 'Truyền Hình'}
                       ],
-            'Cars': [{'name': 'Postal Truck',
-                      'thumb': 'http://www.vidsplay.com/vids/us_postal.jpg',
-                      'video': 'http://www.vidsplay.com/vids/us_postal.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic',
-                      'thumb': 'http://www.vidsplay.com/vids/traffic1.jpg',
-                      'video': 'http://www.vidsplay.com/vids/traffic1.avi',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic Arrows',
-                      'thumb': 'http://www.vidsplay.com/vids/traffic_arrows.jpg',
-                      'video': 'http://www.vidsplay.com/vids/traffic_arrows.mp4',
-                      'genre': 'Cars'}
+            'Phim': [{'name': 'Vị Khách Ngoài Hành Tinh',
+                      'thumb': 'http://download021.fshare.vn/dl/gmvxsmUxjqOHL-7aYCtwq6UWEC5mHv3xX1L2MHRLtc8GvyPBqV6jq-KgjI2MZrks8uUDbo-tn3olFzBm/V%E1%BB%8B%20Kh%C3%A1ch%20Ngo%C3%A0i%20H%C3%A0nh%20Tinh.jpg',
+                      'video': 'http://f-14-vip.vn-hd.com/37546aca196249c58c2fd0c3a4d51471/hdviet/_definst_/smil:mp4_03/store_01_2016/12012016/Impossible_2015_720p_WEBDL_ViE/Impossible_2015_720p_WEBDL_ViE_TM.smil/media_b2500000_4.m3u8',
+                      'genre': 'Music'},
+                     {'name': 'Sử Thi Baahubali',
+                      'thumb': 'http://download006.fshare.vn/dl/-JOnbmNPpp05QI5eobQhaAtHtbDIwibjwE9BHgv+ao0wV8fEFmUP2U7ADLasCDUocACsz1kSQC00WO1s/S%E1%BB%AD%20Thi%20Baahubali.jpg',
+                      'video': 'http://n02.vn-hd.com/556028eb763f015ec57f3b4122d35ef0/082015/20/Baahubali_The_Beginning_2015_1080p_AC3_S/Baahubali_The_Beginning_2015_1080p_AC3_S_1792/playlist.m3u8',
+                      'genre': 'Music'}
+					  ],
+			'Radio': [{'name': 'VOV1',
+                      'thumb': 'http://download031.fshare.vn/dl/n9CV1hrIzx1oKmLnffDd1l-A+5KP7D93IyHge6Kon8e2zAf7X6S6gFc2TQGjgD0OwNIoseiEMHYW1v2t/VOV1.png',
+                      'video': 'http://210.245.60.242:1935/vov1/vov1/playlist.m3u8',
+                      'genre': 'Radio'},
+                     {'name': 'VOV2',
+                      'thumb': 'http://download029.fshare.vn/dl/AkoHoOCS1CDGFpv5PwyFmcGu3+4moLPKsooPppSjk2FodMvAyr6DyyqorCt3r6PP9f3d7A0N-KD74izk/VOV2.png',
+                      'video': 'http://210.245.60.242:1935/vov2/vov2/playlist.m3u8',
+                      'genre': 'Radio'},
+					 {'name': 'VOV3',
+                      'thumb': 'http://download027.fshare.vn/dl/CPn2AhDhAaGwVGWetWY71wmuTzF+e1NW1pTpt74-8wbpFABtMaw7ER2Ft0xXoM3-jEMUkOmqakz86FqI/VOV3.png',
+                      'video': 'http://210.245.60.242:1935/vov3/vov3/playlist.m3u8',
+                      'genre': 'Radio'},
+                     {'name': 'Music Dance',
+                      'thumb': 'http://download029.fshare.vn/dl/ae8BxcEhBg4MkFkWIWJNRdZtirKA8jXZMkJDNm5-kqMt3r6yFHWz9aFRFMj3CowzTC6FsBOntooOKx57/Music%20Dance.jpg',
+                      'video': 'http://95.141.24.61/',
+                      'genre': 'Radio'}		  
                      ],
-            'Food': [{'name': 'Chicken',
-                      'thumb': 'http://www.vidsplay.com/vids/chicken.jpg',
-                      'video': 'http://www.vidsplay.com/vids/bbqchicken.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Hamburger',
-                      'thumb': 'http://www.vidsplay.com/vids/hamburger.jpg',
-                      'video': 'http://www.vidsplay.com/vids/hamburger.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Pizza',
-                      'thumb': 'http://www.vidsplay.com/vids/pizza.jpg',
-                      'video': 'http://www.vidsplay.com/vids/pizza.mp4',
-                      'genre': 'Food'}
-                     ]}
+            'TV Tổng Hợp': [{'name': 'N+LIVE',
+                      'thumb': 'http://download031.fshare.vn/dl/woGoYX-0wA8p002FqkE9CRUg3oiYh5+sxXCEfdHKSmECsqBvyuzBo2SHXLa27NH1UhQRuyILjbnRc2nh/NLive.png',
+                      'video': 'rtmp://123.30.134.108/live/nctlive',
+                      'genre': 'TV Tổng Hợp'},
+                     {'name': 'KeengTV',
+                      'thumb': 'http://download029.fshare.vn/dl/TY7Em00YXu2osC6ejN0J-DMgy0k9f4EuhzZ+cxZeprBAqz1eb6y+CWHBjQSYS0zwn7dVoQc+TwEUDl1y/Keeng%20TV.jpg',
+                      'video': 'http://125.235.29.16/livetv/keeng.m3u8',
+                      'genre': 'TV Tổng Hợp'},
+                     {'name': 'SCTVHD Phim Nước Ngoài',
+                      'thumb': 'http://download029.fshare.vn/dl/Cpj05kJtTqwcw2rZH+sRD99K6-z9PXgxfeVXyKcz1SNVh5eVUYlBQqia084O5hZrZArbXQQTMGth9SpJ/SCTV%20HD%20-%20Phim%20N%C6%B0%E1%BB%9Bc%20Ngo%C3%A0i.png',
+                      'video': 'rtmp://112.197.2.135:1935/colive/C015_HD_2',
+                      'genre': 'TV Tổng Hợp'}
+                     ]}		 
 
 
 def get_categories():
